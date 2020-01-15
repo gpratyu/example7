82482d28e4b3352ae3ea1fa0ad1d3238d37fffee
gpratyu <pratyushagovindaraju@gmail.com>
2020-01-15 14:26:08 -0500

    Corrected indentation

--- convert.py
+++ convert.py
@@ -10,7 +10,7 @@ with open('input.csv','r') as f:
 	rows = list(reader)
 	polls = [dict(row) for row in rows]
 
-print(polls)
+#print(polls)
 
 # 2. write a new file called output.csv and write a row with two headers: "date" and "approve"
 with open('output.csv', 'w') as f:
@@ -18,26 +18,21 @@ with open('output.csv', 'w') as f:
 	writer.writerow(['date', 'approve']) 
 
 # 3. Loop through each row of `polls` 
-for poll in polls:
-	date = poll['enddate']
-	approve = poll['approve']
-	print(date)
-	print(approve)
+	for poll in polls:
+		date = poll['enddate']
+		approve = poll['approve']
+		#print(date)
+		#print(approve)
 
 
-	# 4. and within that loop... convert the format of `enddate` from "1/22/2017" to "22-Jan-17"
-	# hint: to read the date you will need to use
-	raw_date = datetime.datetime.strptime(date, "%m/%d/%Y")
-	
-	date_str = raw_date.strftime("%d-%b-%y") # 01/11/17
-	print(date_str)
-	#
-	#       and to write the date you will need to use something like 
-	#       new_date = date.strftime("insert output format here")
-	# 
-	#       date formats can be found at https://strftime.org/
-	
-	# 5. write a new row of data with the transformed date and value for "approve" 
+		# 4. and within that loop... convert the format of `enddate` from "1/22/2017" to "22-Jan-17"
+		raw_date = datetime.datetime.strptime(date, "%m/%d/%Y")
+		
+		date_str = raw_date.strftime("%d-%b-%y") # 01/11/17
+		print(date_str)
+		
+		# 5. write a new row of data with the transformed date and value for "approve" 
+		writer.writerow([date_str, approve])
 
 
 
